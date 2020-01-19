972f3932b393882d4c5ff39b9a80fe6fb726e0da
jyh <jyh@eastcom.com>
2020-01-19 17:27:01 +0800

    新增“联赛列表”和“事件列表”两个页面。

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,17 @@ Component({
             console.log("SHOW index/index");
             this.getTabBar().setData({nowIndex:0});
         }
+    },
+    methods: {
+        goToRecentEvents() {
+            wx.navigateTo({
+                url: '/pages/events/index',
+            })
+        },
+        goToMyLeagues() {
+            wx.navigateTo({
+                url: '/pages/leagues/index',
+            })
+        }
     }
 })
